522c293f67b92646c1ccdddce01236440efeff0e
Emanuel-Andrei Paius <pandaismyname1@gmail.com>
2025-01-14 07:52:02 +0200

    Farm & Charm Visuals

--- common/src/main/java/com/pandaismyname1/emiletsdocompat/farm_and_charm/SiloRecipe.java
+++ common/src/main/java/com/pandaismyname1/emiletsdocompat/farm_and_charm/SiloRecipe.java
@@ -9,6 +9,7 @@ import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.api.widget.WidgetHolder;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceLocation;
 
 import java.util.function.Supplier;
 
@@ -16,6 +17,8 @@ public class SiloRecipe extends BasicEmiRecipe {
     protected static final Supplier<RegistryAccess> REGISTRY_ACCESS =
             EnvExecutor.getEnvSpecific(() -> () -> () -> GameInstance.getClient().player.level().registryAccess(),
                     () -> () -> () -> GameInstance.getServer().registryAccess());
+    public static final ResourceLocation TEXTURE = new ResourceLocation("farm_and_charm", "textures/gui/silo.png");
+
 
     public SiloRecipe(EmiRecipeCategory category, net.satisfy.farm_and_charm.recipe.SiloRecipe recipe) {
         super(category, recipe.getId(), 70, 18);
@@ -28,16 +31,24 @@ public class SiloRecipe extends BasicEmiRecipe {
 
     @Override
     public void addWidgets(WidgetHolder widgets) {
-        DisplayUtils.CreateWidget(this, widgets, this.inputs, this.outputs);
+        widgets.addTexture(TEXTURE, -1, -1, 124, 60, 25, 12);
+        widgets.addFillingArrow(50, 21, 5000);
+
+        if (this.inputs.size() > 0) {
+            widgets.addSlot(this.inputs.get(0), 23, 21).drawBack(false);
+        }
+        if (this.outputs.size() > 0) {
+            widgets.addSlot(this.outputs.get(0), 83, 21).drawBack(false);
+        }
     }
 
     @Override
     public int getDisplayHeight() {
-        return 40;
+        return 60;
     }
 
     @Override
     public int getDisplayWidth() {
-        return 140;
+        return 124;
     }
 }
